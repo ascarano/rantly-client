e2cc75b7307d38faf8acefe33c281b43493d428f
ascarano <adamscarano@gmail.com>
2015-03-30 12:51:52 -0600

    added config

--- Brocfile.js
+++ Brocfile.js
@@ -4,6 +4,12 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 var app = new EmberApp();
 
+var app = new EmberApp({
+  dotEnv: {
+    clientAllowedKeys: ['ADAPTER_URL']
+  }
+});
+
 // Use `app.import` to add additional libraries to the generated
 // output files.
 //
